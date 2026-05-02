b11c03046def77e930f5ae2af2c6562e1ca8d3a0
WojtekW1 <kolokin1234@gmail.com>
2026-05-02 14:12:42 +0200

    start.sh

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -o errexit
+
+python manage.py migrate
+
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+u = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+e = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+p = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+if u and e and p and not User.objects.filter(username=u).exists():
+    User.objects.create_superuser(username=u, email=e, password=p)
+    print('Created superuser:', u)
+else:
+    print('Superuser exists or env missing')
+"
+
+exec daphne config.asgi:application --bind 0.0.0.0 --port ${PORT}
